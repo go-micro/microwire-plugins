8183365ad1e2c89ec4cf672444e5f574abc03aee
René Jochum <rene@jochum.dev>
2022-10-03 11:22:22 +0200

    Simply find_changes.sh

---
+++ scripts/find_changed.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function go_mods() {
+    local OLD_SHA=$(git rev-list -n 1 ${1})
+    local NEW_SHA=$(git rev-list -n 1 ${2})
+
+    local cfiles=$(git diff --name-only ${OLD_SHA} ${NEW_SHA})
+    for f in ${cfiles}; do
+        if [[ -f "$(dirname ${f})/go.mod" ]]; then
+            echo "$(dirname ${f})"
+        fi
+        if [[ -f "$(dirname ${f}/../go.mod)" ]]; then
+            echo $(dirname ${f}/../)
+        fi
+    done
+}
+
+go_mods $1 $2 | sort -u
