9438b8073524873a9599470bdc59fd273c964b4a
René Jochum <rene@jochum.dev>
2022-10-03 15:28:56 +0200

    feat: Add sets/nats

---
+++ sets/nats/nats.go
@@ -0,0 +1,8 @@
+package nats
+
+import (
+	_ "github.com/go-micro/microwire-plugins/broker/nats/v5"
+	_ "github.com/go-micro/microwire-plugins/cli/urfave/v5"
+	_ "github.com/go-micro/microwire-plugins/registry/nats/v5"
+	_ "github.com/go-micro/microwire-plugins/transport/nats/v5"
+)
