1f946d3d1ac91390d8e1494ced5bd8ed3a9a920c
René Jochum <rene@jochum.dev>
2022-10-03 16:52:46 +0200

    feat(sets/defaults): add

---
+++ sets/defaults/defaults.go
@@ -0,0 +1,8 @@
+package defaults
+
+import (
+	_ "github.com/go-micro/microwire-plugins/broker/http/v5"
+	_ "github.com/go-micro/microwire-plugins/cli/urfave/v5"
+	_ "github.com/go-micro/microwire-plugins/registry/mdns/v5"
+	_ "github.com/go-micro/microwire-plugins/transport/http/v5"
+)
