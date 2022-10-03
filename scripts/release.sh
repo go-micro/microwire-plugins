a0f3a354e135cb1eb129da1050af4e62e7188a1e
René Jochum <rene@jochum.dev>
2022-10-03 13:58:06 +0200

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
 ######################################################################################
-# $ release.sh                                                                       #
+# $ release.sh broker/http                                                           #
 #                                                                                    #
 # Release plugins that have changed it requires to be in a git repo with a clean tag #
 #                                                                                    #
 ######################################################################################
 
-function go_mods() {
-	local OLD_SHA=$(git rev-list -n 1 ${1})
-	local NEW_SHA=$(git rev-list -n 1 ${2})
+function increment_version ()
+{
+    declare -a part=( ${1//\./ } )
+    declare    new
+    declare -i carry=1
 
-	local cfiles=$(git diff --name-only ${OLD_SHA} ${NEW_SHA})
-	for f in ${cfiles}; do
-		if [[ -f "$(dirname ${f})/go.mod" ]]; then
-			echo "$(dirname ${f})"
-		fi
-		if [[ -f "$(dirname ${f}/../go.mod)" ]]; then
-			echo $(dirname ${f}/../)
-		fi
-		if [[ -f "$(dirname ${f}/../../go.mod)" ]]; then
-			echo $(dirname ${f}/../../)
-		fi
-	done
-}
-
+    for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
+        len=${#part[CNTR]}
+        new=$((part[CNTR]+carry))
+        [ ${#new} -gt $len ] && carry=1 || carry=0
+        [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+    done
+    new="${part[*]}"
+    echo -e "${new// /.}"
+} 
 
 function last_tags() {
     local alltags=$(git tag --sort=-creatordate)
     for t in ${alltags}; do
-        if [[ ${t} != *"/"* ]]; then
+        if [[ ${t} == *"${1}"* ]]; then
             echo ${t}
         fi
     done
@@ -37,12 +34,22 @@ function last_tags() {
 }
 
 function release() {
-    local new_tag="$(git tag --points-at HEAD)"
-    local last_tag=$(last_tags | head -n 2 | tail -n 1)
-    local changed_pkgs=$(go_mods ${new_tag} ${last_tag} | sort -u)
-    for pkg in ${changes}; do
-        gh release "${pkg}/${new_tag}" --generate-notes 
-    done
+    if [[ ! -d "${1}" ]]; then
+        echo "Unknown package '${1}' given."
+        exit 1
+    fi
+
+    local pkg="${1}"
+    local last_tag=$(last_tags "${pkg}" | head -n 1)
+    local last_tag_split=(${last_tag//\// })
+    
+    local v_version=${last_tag_split[-1]}
+    local version=${v_version:1}
+
+    local tmp_new_tag="$(printf "/%s" "${last_tag_split[@]::${#last_tag_split[@]}-1}")/v$(increment_version ${version})"
+    local new_tag=${tmp_new_tag:1}
+
+    echo -e "# Run:\ngh release "${pkg}/${new_tag}" --generate-notes"
 }
 
-release
+release "${1}"
